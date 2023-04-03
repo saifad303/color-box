9d2941797d37f3b05eb2be40069526d5af071de9
Nazrul <heynazrul@gmail.com>
2023-04-04 00:22:48 +0600

    added box two updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import BoxOne from './components/BoxOne';
-import './App.css';
-import BoxTwo from './components/BoxTwo/BoxTwo';
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import BoxOne from "./components/BoxOne";
+import BoxTwo from "./components/BoxTwo/BoxTwo";
+import BoxThree from "./components/BoxThree";
+import "./App.css";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +15,7 @@ function App() {
     <div className="App">
       <BoxOne />
       <BoxTwo></BoxTwo>
+      <BoxThree />
     </div>
   );
 }
